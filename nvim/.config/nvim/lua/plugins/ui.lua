c540a4d8714ac8441b7848780c049f1393910110
columbine2k <columbine.k@gmail.com>
2023-08-07 23:02:51 +0800

    23.08.07

--- nvim/.config/nvim/lua/plugins/ui.lua
+++ nvim/.config/nvim/lua/plugins/ui.lua
@@ -13,7 +13,7 @@ return {
     config = function()
       local alpha = require("alpha")
       local dashboard = require("alpha.themes.dashboard")
-
+      -- header
       local header = {
         [[ ⣇⣿⠘⣿⣿⣿⡿⡿⣟⣟⢟⢟⢝⠵⡝⣿⡿⢂⣼⣿⣷⣌⠩⡫⡻⣝⠹⢿⣿⣷ ]],
         [[ ⡆⣿⣆⠱⣝⡵⣝⢅⠙⣿⢕⢕⢕⢕⢝⣥⢒⠅⣿⣿⣿⡿⣳⣌⠪⡪⣡⢑⢝⣇ ]],
@@ -30,8 +30,14 @@ return {
         [[ ⡝⡵⡈⢟⢕⢕⢕⢕⣵⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣶⣿⣿⣿⣿⣿⠿⠋⣀⣈⠙ ]],
         [[ ⡝⡵⡕⡀⠑⠳⠿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⠛⢉⡠⡲⡫⡪⡪⡣ ]],
       }
-
       dashboard.section.header.val = header
+      -- 选项卡
+      dashboard.section.buttons.val = {
+        dashboard.button( "e", "  > New file" , ":ene <BAR> startinsert <CR>"),
+        dashboard.button( "<Leader>ff", "  > Find file", ":FzfLua files<CR>"),
+        dashboard.button( "<Leader>fo", "  > Recent"   , ":FzfLua oldfiles<CR>"),
+        dashboard.button( "<Leader>fw", "  > Find Word", ":FzfLua grep<CR>"),
+      }
       alpha.setup(dashboard.opts)
     end,
   },
