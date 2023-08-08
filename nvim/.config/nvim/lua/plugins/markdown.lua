c210ba224d2773e14fe2439bd05c731a2ec3088e
columbine2k <columbine.k@gmail.com>
2023-08-09 00:05:24 +0800

    23.08.09

--- nvim/.config/nvim/lua/plugins/markdown.lua
+++ nvim/.config/nvim/lua/plugins/markdown.lua
@@ -3,17 +3,17 @@
 return {
   -- markdown 预览
 	{
-	'toppair/peek.nvim',
-	build = 'deno task --quiet build:fast',
-	ft = 'markdown',
-	config = function()
-		vim.api.nvim_create_user_command('PeekOpen', require('peek').open, {})
-		vim.api.nvim_create_user_command('PeekClose', require('peek').close, {})
-		require('peek').setup ({
-			theme = 'light',
-			app = 'browser',
-		})
-	end,
+	  'toppair/peek.nvim',
+	  build = 'deno task --quiet build:fast',
+	  ft = 'markdown',
+	  config = function()
+		  vim.api.nvim_create_user_command('PeekOpen', require('peek').open, {})
+		  vim.api.nvim_create_user_command('PeekClose', require('peek').close, {})
+		  require('peek').setup ({
+			  theme = 'light',
+			  app = 'browser',
+		  })
+	  end,
 	},
   -- markdown wiki
   {
@@ -34,6 +34,15 @@ return {
       vim.g.vimwiki_markdown_link_ext = 1
       vim.g.taskwiki_markdown_syntax = "markdown"
       vim.g.indentLine_conceallevel = 2
+    end,
+  },
+  -- 表格格式化
+  {
+    'dhruvasagar/vim-table-mode',
+    ft = { "markdown", "vimwiki" },
+    config = function()
+			vim.g.table_mode_corner = '|'
+			vim.keymap.set("n", "\\f", ":TableModeToggle<CR>", { noremap = true })
     end,
   },
 }
