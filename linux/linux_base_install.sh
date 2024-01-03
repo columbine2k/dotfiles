cd664c9ef2087fa89fb629e956e7e8c957ab64cb
soh_linux <columbine.k@gmail.com>
2024-01-03 09:56:30 +0800

    23.01.03

--- linux/linux_base_install.sh
+++ linux/linux_base_install.sh
@@ -10,33 +10,33 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/
 git clone https://github.com/Aloxaf/fzf-tab ~/.oh-my-zsh/custom/plugins/fzf-tab
 
 # === 字体 ===
-pacman -S ttf-ibmplex-mono-nerd ttf-jetbrains-mono-nerd adobe-source-han-sans-cn-fonts adobe-source-code-pro-fonts
+sudo pacman -S ttf-ibmplex-mono-nerd ttf-jetbrains-mono-nerd adobe-source-han-sans-cn-fonts adobe-source-code-pro-fonts
 
 # === 终端应用 ===
 # rg
-pacman -S ripgrep
+sudo pacman -S ripgrep
 # fd
-pacman -S fd
+sudo pacman -S fd
 # ag 
-pacman -S the_silver_searcher
+sudo pacman -S the_silver_searcher
 # fzf
-pacman -S fzf
+sudo pacman -S fzf
 # neofetch
-pacman -S neofetch
+sudo pacman -S neofetch
 # bat
-pacman -S bat
+sudo pacman -S bat
 # joshuto
-paru -S joshuto
+#paru -S joshuto
 # tree
-pacman -S tree
+sudo pacman -S tree
 # lazygit
-pacman -S lazygit
+sudo pacman -S lazygit
 # stow
-pacman -S stow
+sudo pacman -S stow
 # bottom
-pacman -S bottom
+sudo pacman -S bottom
 # go-musicfox
-paru -S go-musicfox-bin
+#paru -S go-musicfox-bin
 
 # === 桌面环境 ===
-pacman -S hyprland kitty waybar
+sudo pacman -S hyprland kitty waybar
